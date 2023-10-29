f5135206733dbf91f5ad0fc78c8e35ab89a880b0
Tutku Ince <tutku.ince@outlook.com>
2023-10-29 22:51:11 +0300

    initial commit

---
+++ src/test/java/io/ince/springbootunittesting/SpringBootUnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ince.springbootunittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
