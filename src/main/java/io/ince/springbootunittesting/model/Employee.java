8c921677f66fff0ab39c5eee6516111e14622db6
Tutku Ince <tutku.ince@outlook.com>
2023-11-17 23:32:19 +0300

    Employee entity class has been created

--- src/main/java/io/ince/springbootunittesting/model/Employee.java
+++ src/main/java/io/ince/springbootunittesting/model/Employee.java
@@ -1,2 +1,28 @@
-package io.ince.springbootunittesting.model;public class Employee {
+package io.ince.springbootunittesting.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
 }
