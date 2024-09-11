cbd440e157f5ce726767e1744afcb79e89e4e13d
Tutku Ince <tutku.ince@outlook.com>
2024-09-11 17:18:47 +0300

    JUnit test for saveEmployee method

---
+++ spring-boot-testing/src/test/java/com/incetutku/springboottesting/service/EmployeeServiceTest.java
@@ -0,0 +1,46 @@
+package com.incetutku.springboottesting.service;
+
+import com.incetutku.springboottesting.model.Employee;
+import com.incetutku.springboottesting.repository.EmployeeRepository;
+import com.incetutku.springboottesting.service.impl.EmployeeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+class EmployeeServiceTest {
+
+    private EmployeeRepository employeeRepository;
+    private EmployeeService employeeService;
+
+    @BeforeEach
+    void setUp() {
+        employeeRepository = mock(EmployeeRepository.class);
+        employeeService = new EmployeeServiceImpl(employeeRepository);
+    }
+
+    @DisplayName("JUnit test for saveEmployee method")
+    @Test
+    void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject() {
+        // given - precondition or setup
+        Employee employee = Employee.builder()
+                .id(1L)
+                .name("Tutku")
+                .surname("Ince")
+                .email("ti@mail.com")
+                .build();
+        given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.empty());
+        given(employeeRepository.save(employee)).willReturn(employee);
+
+        // when - action or the behaviour that we are going to test
+        Employee savedEmployee = employeeService.saveEmployee(employee);
+
+        // then - verify the output
+        assertThat(savedEmployee).isNotNull();
+    }
+}
