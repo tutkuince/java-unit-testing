83a4edbba3a664a959a6fad1a634c17147454d16
Tutku Ince <tutku.ince@outlook.com>
2024-09-12 23:35:45 +0300

    Junit test for Get All Employees REST API

--- spring-boot-testing/src/test/java/com/incetutku/springboottesting/controller/EmployeeControllerTest.java
+++ spring-boot-testing/src/test/java/com/incetutku/springboottesting/controller/EmployeeControllerTest.java
@@ -1,6 +1,5 @@
 package com.incetutku.springboottesting.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.incetutku.springboottesting.model.Employee;
 import com.incetutku.springboottesting.service.EmployeeService;
@@ -9,20 +8,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest
@@ -48,7 +47,7 @@ class EmployeeControllerTest {
                 .build();
     }
 
-    @DisplayName("Junit test for create employee operation")
+    @DisplayName("Junit test for create employee REST API")
     @Test
     void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
         // given - precondition or setup
@@ -67,6 +66,28 @@ class EmployeeControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.surname", CoreMatchers.is(employee.getSurname())));
         response.andExpect(MockMvcResultMatchers.status().isCreated())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(employee.getEmail())));
+    }
+
+    @DisplayName("Junit test for Get All Employees REST API")
+    @Test
+    void givenListOfEmployees_whenGetAllEmployees_thenReturnEmployeeList() throws Exception {
+        // given - precondition or setup
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(employee);
+        employeeList.add(Employee.builder()
+                .name("Utku")
+                .surname("Ince")
+                .email("utku@mail.com")
+                .build());
+        given(employeeService.getAllEmployees()).willReturn(employeeList);
+
+        // when - action or the behaviour that we are going to test
+        ResultActions response = mockMvc.perform(get("/api/v1/employees"));
+
+        // then - verify the output
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(employeeList.size())));
     }
 
 }
