be005fabc961d7d2d3cbde1bf014e006ecff9916
Tutku Ince <tutku.ince@outlook.com>
2024-09-15 18:16:13 +0300

    Integration test for create employee REST API

--- spring-boot-testing/src/test/java/com/incetutku/springboottesting/integration/EmployeeControllerITest.java
+++ spring-boot-testing/src/test/java/com/incetutku/springboottesting/integration/EmployeeControllerITest.java
@@ -1,12 +1,24 @@
 package com.incetutku.springboottesting.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.incetutku.springboottesting.model.Employee;
 import com.incetutku.springboottesting.repository.EmployeeRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -24,5 +36,30 @@ public class EmployeeControllerITest {
     @BeforeEach
     void setUp() {
         employeeRepository.deleteAll();
+    }
+
+    @DisplayName("Integration test for create employee REST API")
+    @Test
+    void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
+        // given - precondition or setup
+        Employee employee = Employee.builder()
+                .id(1L)
+                .name("Tutku")
+                .surname("Ince")
+                .email("ti@mail.com")
+                .build();
+
+        // when - action or the behaviour that we are going to test
+        ResultActions response = mockMvc.perform(post("/api/v1/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employee)));
+
+        // then - verify the output
+        response.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", is(employee.getName())));
+        response.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.surname", is(employee.getSurname())));
+        response.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.email", is(employee.getEmail())));
     }
 }
