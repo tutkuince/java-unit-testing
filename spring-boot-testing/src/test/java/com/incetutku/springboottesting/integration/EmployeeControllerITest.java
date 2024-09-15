da5a529d506df6c3a99eb5eecd7563517c4877d0
Tutku Ince <tutku.ince@outlook.com>
2024-09-15 20:35:42 +0300

    Integration test for Get All Employees REST API

--- spring-boot-testing/src/test/java/com/incetutku/springboottesting/integration/EmployeeControllerITest.java
+++ spring-boot-testing/src/test/java/com/incetutku/springboottesting/integration/EmployeeControllerITest.java
@@ -13,9 +13,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -28,27 +32,29 @@ public class EmployeeControllerITest {
     // Injecting MockMvc class to make HTTP request using perform() method.
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
     private EmployeeRepository employeeRepository;
     @Autowired
     private ObjectMapper objectMapper;
 
+    private Employee employee;
+
     @BeforeEach
     void setUp() {
         employeeRepository.deleteAll();
+        employee = Employee.builder()
+                .id(1L)
+                .name("Tutku")
+                .surname("Ince")
+                .email("ti@mail.com")
+                .build();
     }
 
     @DisplayName("Integration test for create employee REST API")
     @Test
     void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
         // given - precondition or setup
-        Employee employee = Employee.builder()
-                .id(1L)
-                .name("Tutku")
-                .surname("Ince")
-                .email("ti@mail.com")
-                .build();
+        // employee
 
         // when - action or the behaviour that we are going to test
         ResultActions response = mockMvc.perform(post("/api/v1/employees")
@@ -62,5 +68,27 @@ public class EmployeeControllerITest {
                 .andExpect(jsonPath("$.surname", is(employee.getSurname())));
         response.andDo(print()).andExpect(status().isCreated())
                 .andExpect(jsonPath("$.email", is(employee.getEmail())));
+    }
+
+    @DisplayName("Integration test for Get All Employees REST API")
+    @Test
+    void givenListOfEmployees_whenGetAllEmployees_thenReturnEmployeeList() throws Exception {
+        // given - precondition or setup
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(employee);
+        employeeList.add(Employee.builder()
+                .name("Utku")
+                .surname("Ince")
+                .email("utku@mail.com")
+                .build());
+        employeeRepository.saveAll(employeeList);
+
+        // when - action or the behaviour that we are going to test
+        ResultActions response = mockMvc.perform(get("/api/v1/employees"));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()", is(employeeList.size())));
     }
 }
