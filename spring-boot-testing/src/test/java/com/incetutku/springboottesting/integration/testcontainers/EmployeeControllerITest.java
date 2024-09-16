a6b306c5095575ca18e31fcd05c94946e0c1f1aa
Tutku Ince <tutku.ince@outlook.com>
2024-09-16 21:47:16 +0300

    using @DynamicPropertySource

--- spring-boot-testing/src/test/java/com/incetutku/springboottesting/integration/testcontainers/EmployeeControllerITest.java
+++ spring-boot-testing/src/test/java/com/incetutku/springboottesting/integration/testcontainers/EmployeeControllerITest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -40,6 +42,13 @@ public class EmployeeControllerITest {
     private ObjectMapper objectMapper;
 
     private Employee employee;
+
+    @DynamicPropertySource
+    public static void dynamicPropertySource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
 
     @BeforeAll
     static void beforeAll() {
