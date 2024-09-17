3b85a7d7b9fa04e597d2852e05977de743df2514
Tutku Ince <tutku.ince@outlook.com>
2024-09-17 19:58:14 +0300

    Write Integration Test StudentRepository using Testcontainers

--- student-service/src/test/java/com/incetutku/studentservice/integration/StudentRepositoryITest.java
+++ student-service/src/test/java/com/incetutku/studentservice/integration/StudentRepositoryITest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class StudentRepositoryITest {
+class StudentRepositoryITest extends AbstractContainerBaseTest {
 
     @Autowired
     private StudentRepository studentRepository;
