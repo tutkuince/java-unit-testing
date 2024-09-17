6f22b356d2e4a9c7027ecc5e6eba9b3dc22eb125
Tutku Ince <tutku.ince@outlook.com>
2024-09-17 19:29:52 +0300

    update

--- student-service/src/main/java/com/incetutku/studentservice/model/Student.java
+++ student-service/src/main/java/com/incetutku/studentservice/model/Student.java
@@ -1,8 +1,6 @@
 package com.incetutku.studentservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -14,6 +12,7 @@ import lombok.*;
 @Table(name = "students")
 public class Student {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String surname;
