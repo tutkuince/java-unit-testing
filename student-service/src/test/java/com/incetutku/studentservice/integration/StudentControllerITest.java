1377b933fadd93794c2eb9b69642a47420d6b5a1
Tutku Ince <tutku.ince@outlook.com>
2024-09-17 19:30:56 +0300

    update

--- student-service/src/test/java/com/incetutku/studentservice/integration/StudentControllerITest.java
+++ student-service/src/test/java/com/incetutku/studentservice/integration/StudentControllerITest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.incetutku.studentservice.model.Student;
 import com.incetutku.studentservice.repository.StudentRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -34,8 +39,6 @@ public class StudentControllerITest {
 
     @BeforeEach
     void setUp() {
-        studentRepository.deleteAll();
-
         student = Student.builder()
                 .name("Tutku")
                 .surname("Ince")
@@ -43,11 +46,17 @@ public class StudentControllerITest {
                 .build();
     }
 
-    @DisplayName("Integration for Create Student REST API")
+    @AfterEach
+    void tearDown() {
+        studentRepository.deleteAll();
+    }
+
+    @DisplayName("Integration test for Create Student REST API")
     @Test
     void givenStudentObject_whenCreateStudent_thenReturnSavedStudent() throws Exception {
         // given - precondition or setup
         // student
+        List<Student> all = studentRepository.findAll();
 
         // when - action or the behaviour that we are going to test
         ResultActions response = mockMvc.perform(post("/api/v1/students")
@@ -61,4 +70,26 @@ public class StudentControllerITest {
         response.andDo(print()).andExpect(status().isCreated()).andExpect(jsonPath("$.email", is(student.getEmail())));
     }
 
+    @DisplayName("Integration test for Get All Students REST API")
+    @Test
+    void givenListOfStudent_whenGetAllStudents_thenReturnStudentList() throws Exception {
+        // given - precondition or setup
+        List<Student> all = studentRepository.findAll();
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(student);
+        studentList.add(Student.builder()
+                .name("Utku")
+                .surname("Ince")
+                .email("ui@mail.com")
+                .build());
+        studentRepository.saveAll(studentList);
+
+        // when - action or the behaviour that we are going to test
+        ResultActions response = mockMvc.perform(get("/api/v1/students"));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()", is(studentList.size())));
+    }
 }
